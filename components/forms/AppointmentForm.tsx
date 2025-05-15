3001f05637c2c09f8391485c2876d75d3b02e1f9
RebecaTI <mebuffbeka@gmail.com>
2025-05-15 09:38:09 -0300

    fix: simplify setOpen call in AppointmentForm after updating appointment

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -99,7 +99,7 @@ const AppointmentForm = ({
         const updatedAppointment = await updateAppointment(appointmentToUpdate);
 
         if (updatedAppointment) {
-          setOpen && setOpen(false);
+          setOpen(false);
           form.reset();
         }
       }
